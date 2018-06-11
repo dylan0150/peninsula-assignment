8a40ece282970244c43dccc3e24b6b9365425707
dylan0150 <dylan.hanner@yahoo.co.uk>
2018-06-11 11:12:18 +0100

    Made some of the changes pending feedback such as removing the api and moving everything into a service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express    = require('express')
-const bodyParser = require('body-parser')
 const app        = express()
+const PORT       = 8080
 
 let port;
 if ( process.argv.length > 2 && !isNaN( process.argv[2] ) ) {
@@ -10,20 +10,4 @@ if ( process.argv.length > 2 && !isNaN( process.argv[2] ) ) {
 }
 
 app.use(express.static('www'))
-app.use(bodyParser.json())
-
-app.options('*', function(request, response) {
-    response.end()
-})
-app.post('/api/files', function(request, response) {
-    const endpoint = require('./api/files')
-    response.send( endpoint.post(request) )
-    response.end()
-})
-app.get('/api/files', function(request, response) {
-    const endpoint = require('./api/files')
-    response.send( endpoint.get(request) )
-    response.end()
-})
-
-app.listen(port)
+app.listen(PORT)
--- www/js/service/files.js
+++ www/js/service/files.js
@@ -2,17 +2,44 @@ angular.module('service.files',[])
 
 .service('files', function($http) {
 
+    function recursiveSearch(files, search_param) {
+        return files.filter(function(e, i) {
+            if ( e.name != undefined && e.name.toLowerCase().indexOf(search_param.toLowerCase()) > -1 ) {
+                return true
+            }
+            if ( e.type != undefined && e.type.toLowerCase().indexOf(search_param.toLowerCase()) > -1 ) {
+                return true
+            }
+            if ( e.files != undefined ) {
+                e.files = recursiveSearch(e.files, search_param)
+                if (e.files.length > 0) {
+                    e.expanded = true
+                    return true
+                }
+            }
+            return false
+        })
+    }
+
     this.get = function() {
-        return $http.get('/api/files').then(function(response) {
-            return response.data
+        return $http.get("files.json").then(function(response) {
+            return {
+                name: "/",
+                type: "folder",
+                expanded: true,
+                files: response.data
+            }
         })
     }
 
     this.filter = function(form, headers) {
-        return $http.post('/api/files',form,headers).then(function(response) {
-            return response.data
-        }).catch(function(err) {
-            if ( err.data == null ) { console.debug("Safely cancelled request") }
+        return $http.get("files.json").then(function(response) {
+            return {
+                name: "/",
+                type: "folder",
+                expanded: true,
+                files: recursiveSearch(response.data, form.search)
+            }
         })
     }
 
